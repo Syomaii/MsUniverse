94fe8725297904ab9961f4dd12441e67a0af0e0e
Syomai <126589688+Syomaii@users.noreply.github.com>
2023-05-20 17:46:15 +0800

    Add files via upload

---
+++ MsUniverse/MsUniverse/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace MsUniverse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrintT10 = new System.Windows.Forms.Button();
+            this.btnNextT10 = new System.Windows.Forms.Button();
+            this.btnNextT3 = new System.Windows.Forms.Button();
+            this.btnNextT1 = new System.Windows.Forms.Button();
+            this.btnPrintT3 = new System.Windows.Forms.Button();
+            this.btnPrintT1 = new System.Windows.Forms.Button();
+            this.lbTop3 = new System.Windows.Forms.ListBox();
+            this.lbTop20 = new System.Windows.Forms.ListBox();
+            this.lbTop10 = new System.Windows.Forms.ListBox();
+            this.lbTop1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPrintT10
+            // 
+            this.btnPrintT10.Location = new System.Drawing.Point(301, 421);
+            this.btnPrintT10.Name = "btnPrintT10";
+            this.btnPrintT10.Size = new System.Drawing.Size(62, 32);
+            this.btnPrintT10.TabIndex = 0;
+            this.btnPrintT10.Text = "Print";
+            this.btnPrintT10.UseVisualStyleBackColor = true;
+            this.btnPrintT10.Click += new System.EventHandler(this.btnPrintT10_Click);
+            // 
+            // btnNextT10
+            // 
+            this.btnNextT10.Location = new System.Drawing.Point(200, 253);
+            this.btnNextT10.Name = "btnNextT10";
+            this.btnNextT10.Size = new System.Drawing.Size(53, 32);
+            this.btnNextT10.TabIndex = 0;
+            this.btnNextT10.Text = ">";
+            this.btnNextT10.UseVisualStyleBackColor = true;
+            this.btnNextT10.Click += new System.EventHandler(this.btnNextT10_Click);
+            // 
+            // btnNextT3
+            // 
+            this.btnNextT3.Location = new System.Drawing.Point(415, 253);
+            this.btnNextT3.Name = "btnNextT3";
+            this.btnNextT3.Size = new System.Drawing.Size(53, 32);
+            this.btnNextT3.TabIndex = 0;
+            this.btnNextT3.Text = ">";
+            this.btnNextT3.UseVisualStyleBackColor = true;
+            this.btnNextT3.Click += new System.EventHandler(this.btnNextT3_Click);
+            // 
+            // btnNextT1
+            // 
+            this.btnNextT1.Location = new System.Drawing.Point(639, 253);
+            this.btnNextT1.Name = "btnNextT1";
+            this.btnNextT1.Size = new System.Drawing.Size(53, 32);
+            this.btnNextT1.TabIndex = 0;
+            this.btnNextT1.Text = ">";
+            this.btnNextT1.UseVisualStyleBackColor = true;
+            this.btnNextT1.Click += new System.EventHandler(this.btnNextT1_Click);
+            // 
+            // btnPrintT3
+            // 
+            this.btnPrintT3.Location = new System.Drawing.Point(524, 421);
+            this.btnPrintT3.Name = "btnPrintT3";
+            this.btnPrintT3.Size = new System.Drawing.Size(53, 32);
+            this.btnPrintT3.TabIndex = 0;
+            this.btnPrintT3.Text = "Print";
+            this.btnPrintT3.UseVisualStyleBackColor = true;
+            this.btnPrintT3.Click += new System.EventHandler(this.btnPrintT3_Click);
+            // 
+            // btnPrintT1
+            // 
+            this.btnPrintT1.Location = new System.Drawing.Point(744, 421);
+            this.btnPrintT1.Name = "btnPrintT1";
+            this.btnPrintT1.Size = new System.Drawing.Size(53, 32);
+            this.btnPrintT1.TabIndex = 0;
+            this.btnPrintT1.Text = "Print";
+            this.btnPrintT1.UseVisualStyleBackColor = true;
+            this.btnPrintT1.Click += new System.EventHandler(this.btnPrintT1_Click);
+            // 
+            // lbTop3
+            // 
+            this.lbTop3.FormattingEnabled = true;
+            this.lbTop3.ItemHeight = 16;
+            this.lbTop3.Location = new System.Drawing.Point(491, 141);
+            this.lbTop3.Name = "lbTop3";
+            this.lbTop3.Size = new System.Drawing.Size(128, 260);
+            this.lbTop3.TabIndex = 1;
+            this.lbTop3.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbTop3_MouseDoubleClick);
+            // 
+            // lbTop20
+            // 
+            this.lbTop20.FormattingEnabled = true;
+            this.lbTop20.ItemHeight = 16;
+            this.lbTop20.Location = new System.Drawing.Point(52, 141);
+            this.lbTop20.Name = "lbTop20";
+            this.lbTop20.Size = new System.Drawing.Size(128, 260);
+            this.lbTop20.TabIndex = 1;
+            this.lbTop20.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbTop20_MouseDoubleClick);
+            // 
+            // lbTop10
+            // 
+            this.lbTop10.FormattingEnabled = true;
+            this.lbTop10.ItemHeight = 16;
+            this.lbTop10.Location = new System.Drawing.Point(268, 141);
+            this.lbTop10.Name = "lbTop10";
+            this.lbTop10.Size = new System.Drawing.Size(128, 260);
+            this.lbTop10.TabIndex = 1;
+            this.lbTop10.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbTop10_MouseDoubleClick);
+            // 
+            // lbTop1
+            // 
+            this.lbTop1.FormattingEnabled = true;
+            this.lbTop1.ItemHeight = 16;
+            this.lbTop1.Location = new System.Drawing.Point(707, 141);
+            this.lbTop1.Name = "lbTop1";
+            this.lbTop1.Size = new System.Drawing.Size(128, 260);
+            this.lbTop1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(511, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Top 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(276, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Top 10";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(719, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 36);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Top 1";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(0, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(100, 23);
+            this.lblResult.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 36);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Top 20";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(237, 35);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(109, 36);
+            this.lblRes.TabIndex = 2;
+            this.lblRes.Text = "Top 20";
+            this.lblRes.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 532);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTop1);
+            this.Controls.Add(this.lbTop10);
+            this.Controls.Add(this.lbTop20);
+            this.Controls.Add(this.lbTop3);
+            this.Controls.Add(this.btnPrintT1);
+            this.Controls.Add(this.btnPrintT3);
+            this.Controls.Add(this.btnNextT1);
+            this.Controls.Add(this.btnNextT3);
+            this.Controls.Add(this.btnNextT10);
+            this.Controls.Add(this.btnPrintT10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrintT10;
+        private System.Windows.Forms.Button btnNextT10;
+        private System.Windows.Forms.Button btnNextT3;
+        private System.Windows.Forms.Button btnNextT1;
+        private System.Windows.Forms.Button btnPrintT3;
+        private System.Windows.Forms.Button btnPrintT1;
+        private System.Windows.Forms.ListBox lbTop3;
+        private System.Windows.Forms.ListBox lbTop20;
+        private System.Windows.Forms.ListBox lbTop10;
+        private System.Windows.Forms.ListBox lbTop1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ MsUniverse/MsUniverse/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MsUniverse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNextT10_Click(object sender, EventArgs e)
+        {
+            string item = lbTop20.SelectedItem.ToString();
+            if (lbTop10.Items.Count < 10 && !lbTop10.Items.Contains(item))
+                lbTop10.Items.Add(item);
+            else if (lbTop10.Items.Contains(item))
+            {
+
+                MessageBox.Show("Error! you already have this country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNextT1_Click(object sender, EventArgs e)
+        {
+            string item = lbTop3.SelectedItem.ToString();
+            if (lbTop1.Items.Count < 1 && !lbTop1.Items.Contains(item))
+            {
+                lbTop1.Items.Add(item);
+                lblRes.Visible = true;
+                lblRes.Text = "Ms. Universe is Ms. " + item;
+            }
+            else if (lbTop1.Items.Contains(item))
+            {
+                MessageBox.Show("Error! you already have this country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNextT3_Click(object sender, EventArgs e)
+        {
+            string item = lbTop10.SelectedItem.ToString();
+            if (lbTop3.Items.Count < 3 && !lbTop3.Items.Contains(item))
+                lbTop3.Items.Add(item);
+            else if (lbTop3.Items.Contains(item))
+            {
+
+                MessageBox.Show("Error! you already have this country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lbTop20_MouseDoubleClick(object sender, MouseEventArgs e) 
+        { 
+            string item = lbTop20.SelectedItem.ToString();
+            if (lbTop10.Items.Count < 10 && !lbTop10.Items.Contains(item))
+                lbTop10.Items.Add(item);
+            else if (lbTop10.Items.Contains(item))
+            {
+                
+                MessageBox.Show("Error! you already have this country", "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }    
+        }
+
+        private void lbTop10_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string item = lbTop10.SelectedItem.ToString();
+            if (lbTop3.Items.Count < 3 && !lbTop3.Items.Contains(item))
+                lbTop3.Items.Add(item);
+            else if (lbTop3.Items.Contains(item))
+            {
+
+                MessageBox.Show("Error! you already have this country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lbTop3_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string item = lbTop3.SelectedItem.ToString();
+            if (lbTop1.Items.Count < 1 && !lbTop1.Items.Contains(item))
+            {
+                lbTop1.Items.Add(item);
+                lblRes.Visible = true;
+                lblRes.Text = "Ms. Universe is " + item;
+            }
+            else if (lbTop1.Items.Contains(item))
+            {
+                MessageBox.Show("Error! you already have this country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] top20 = File.ReadAllLines(@"top20.txt");
+            foreach (string line in top20)
+            { 
+                lbTop20.Items.Add(line);
+            }
+        }
+
+        private void btnPrintT10_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"top10.txt", "");
+            using (StreamWriter writer = new StreamWriter(@"top10.txt"))
+            {
+                foreach (string lines in lbTop10.Items)
+                {
+                    writer.WriteLine(lines);
+                }
+            }
+        }
+
+        private void btnPrintT3_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"top3.txt", "");
+            using (StreamWriter writer = new StreamWriter(@"top3.txt"))
+            {
+                foreach (string lines in lbTop3.Items)
+                {
+                    writer.WriteLine(lines);
+                }
+            }
+        }
+
+        private void btnPrintT1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"MsUniverse.txt", "");
+            using (StreamWriter writer = new StreamWriter(@"MsUniverse.txt"))
+            {
+                foreach (string lines in lbTop1.Items)
+                {
+                    writer.WriteLine(lines);
+                }
+            }
+        }
+    }
+}
